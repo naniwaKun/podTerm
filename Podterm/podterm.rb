51282c0dfbb0be09b81561d68a0347b73c33f731
naniwa <radio.naniwa@gmail.com>
2015-02-18 15:13:55 +0900

    ループの処理をやめた。mplayerではなく、mpvを使うことにした

--- Podterm/podterm.rb
+++ Podterm/podterm.rb
@@ -4,11 +4,19 @@ require 'rss'
 require "json"
 
 def addpodcast(filename,url)
+
+rss = RSS::Parser.parse(url)
+profile = {}
+
+profile['url'] = url
+profile['title'] = rss.channel.title.gsub(/(\s)/,"")
+profile['des'] = rss.channel.description.gsub(/(\s)/,"").gsub(/<\/?[^>]*>/, "")
+
   urls = Array.new()
   if File.exist?(filename) then
     File.open(filename,"r") do |file|
       urls =  JSON.parse(file.read)
-      urls.push(url)
+      urls.push(profile)
       urls.uniq!
     end
 
@@ -17,51 +25,56 @@ def addpodcast(filename,url)
     end
 
   else
-    urls = [url]
     File.open(filename,"w") do |file|
+      urls.push(profile)
       file.puts  JSON.dump(urls)
     end
   end
+
 end
 
 def viewpodcast(filename)
-  urls = Array.new()
+  profiles = Array.new()
   if File.exist?(filename) then
     File.open(filename,"r") do |file|
-      urls =  JSON.parse(file.read)
+      profiles =  JSON.parse(file.read)
     end
   end
 
-  urls.each_with_index{|podcast , index|
-    rss = RSS::Parser.parse(podcast)
+  profiles.each_with_index{|podcast , index|
+    rss = RSS::Parser.parse(podcast["url"])
     print "\e[35m"
     print "No.#{index+1}"
     puts "\e[0m"
-    puts "Title:\t\t" + rss.channel.title.gsub(/(\s)/,"")
-    	puts "Description:\t" + rss.channel.description.gsub(/(\s)/,"")
+    puts "Title:\t\t" + podcast["title"]
+    	puts "Description:\t" + podcast["des"]
   }
     puts
   print "\e[35m"
   print "何番のポッドキャストを聞きますか？(半角数字のみ)："  
   print "\e[0m"
   num = gets.chomp
-  urls[num.to_i - 1 ]
+
+  profiles[num.to_i - 1 ]["url"]
 end
 
 def viewItems(url)
   rss = RSS::Parser.parse(url)
+  num = rss.items.length
+  rss.items.reverse!
+
   rss.items.each_with_index{|item , index|
     print "\e[35m"
 if  (defined? (item.enclosure.url) )
-    print "No.#{index+1}"
+    print "No.#{num - index}"
 else 
-    print "No.#{index+1}"
+    print "No.#{num - index}"
     print "\e[33m"
     print "音声ファイルがないようです。。"
 end
 puts "\e[0m"
     puts "Title:\t\t" + item.title.gsub(/(\s)/,"")
-    puts "Description:\t" + item.description.gsub(/(\s)/,"")
+    puts "Description:\t" + item.description.gsub(/(\s)/,"").gsub(/<\/?[^>]*>/, "")
   }
     puts
   print "\e[35m"
